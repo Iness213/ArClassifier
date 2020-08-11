d386a2476b12b06b675ca6458187c34821605d97
ilyes24 <ilyes24ghomrani@gmail.com>
2020-08-11 17:24:54 +0100

    Adding new urls

--- ArClassifier/urls.py
+++ ArClassifier/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/', views.index, name='index'),
+    path('home/', views.index, name='home'),
+    path('datasets/', views.datasets, name='datasets'),
+    path('projects/', views.projects, name='projects'),
+    path('classification/', views.classification, name='classification'),
+    path('savedResults/', views.saved_results, name='savedResults'),
+    path('help/', views.help, name='help'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
