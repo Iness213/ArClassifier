4b1736ec2b3397e2a12ec328e6295707e83f4bed
ilyes24 <ilyes24ghomrani@gmail.com>
2020-08-13 14:22:00 +0100

    updating the models

--- ArClassifier/models.py
+++ ArClassifier/models.py
@@ -1,5 +1,8 @@
+import os
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class MyUser(AbstractUser):
@@ -13,13 +16,27 @@ class MyUser(AbstractUser):
 class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    creation_date = models.DateField()
     owner = models.ForeignKey(MyUser, related_name='user_owner', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.creation_date = datetime.now()
+        super(Project, self).save(*args, **kwargs)
 
 
 class Dataset(models.Model):
     name = models.CharField(max_length=255)
     path = models.CharField(max_length=255)
+    creation_date = models.DateField()
     project = models.ForeignKey(Project, related_name='project_project', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.creation_date = datetime.now()
+        super(Dataset, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        os.remove(self.path)
+        super(Dataset, self).delete(*args, **kwargs)
 
 
 class Keyword(models.Model):
