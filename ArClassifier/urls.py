734f1b6a28e05d86860d5be3763c5e9e64ecfe9c
ilyes24 <ilyes24ghomrani@gmail.com>
2020-08-13 14:24:41 +0100

    reconstructing the urls

--- ArClassifier/urls.py
+++ ArClassifier/urls.py
@@ -5,11 +5,21 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.index, name='home'),
-    path('datasets/', views.datasets, name='datasets'),
+
     path('projects/', views.projects, name='projects'),
+    path('project/<int:id>', views.project, name='project'),
+    path('addProject/', views.add_project, name='addProject'),
+    path('deleteProject/<int:id>', views.delete_project, name='deleteProject'),
+
+    path('datasets/', views.datasets, name='datasets'),
+    path('deleteFile/<int:id>', views.delete_file, name='deleteFile'),
+
+    path('feed_text/', views.feed_text, name='feed_text'),
+    path('upload_file/', views.upload_file, name='upload_file'),
     path('classification/', views.classification, name='classification'),
     path('savedResults/', views.saved_results, name='savedResults'),
     path('help/', views.help, name='help'),
+
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
