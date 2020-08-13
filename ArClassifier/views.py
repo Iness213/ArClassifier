71bd2bd6b3cecb6abc73aa25f1cf1b8585274a57
ilyes24 <ilyes24ghomrani@gmail.com>
2020-08-13 14:24:25 +0100

    updatong the views

--- ArClassifier/views.py
+++ ArClassifier/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model, authenticate, login, logout
+
 from ArClassifier.form import LogInForm, SignUpForm, ForgotPassForm
 from ArClassifier.models import MyUser, Project, Dataset, Keyword, Result
 
@@ -18,15 +19,15 @@ def index(request):
         number_of_keywords = 0
         for project in user_projects_qs:
             project_datasets_qs = Dataset.objects.filter(project=project)
-            number_of_datasets = number_of_datasets + project_datasets_qs.len()
+            number_of_datasets = number_of_datasets + len(project_datasets_qs)
 
             for dataset in project_datasets_qs:
                 dataset_keywords_qs = Keyword.objects.filter(dataset=dataset)
-                number_of_keywords = number_of_keywords + dataset_keywords_qs.len()
+                number_of_keywords = number_of_keywords + len(dataset_keywords_qs)
 
                 for keyword in dataset_keywords_qs:
                     keyword_results_qs = Result.objects.filter(keyword=keyword)
-                    number_of_saved_results = number_of_saved_results + keyword_results_qs.len()
+                    number_of_saved_results = number_of_saved_results + len(keyword_results_qs)
         context = {
             'number_of_datasets': number_of_datasets,
             'number_of_saved_results': number_of_saved_results,
@@ -39,14 +40,44 @@ def index(request):
 
 @login_required(login_url='login/')
 def projects(request):
-    context = {}
+    user_email = request.session['user_email']
+    curernt_user = MyUser.objects.filter(email__exact=user_email).first()
+    user_projects_qs = Project.objects.filter(owner=curernt_user)
+    context = {
+        'projects': user_projects_qs
+    }
     return render(request, 'projects.html', context)
+
+
+@login_required(login_url='login/')
+def project(request, id):
+    prjct = Project.objects.get(id=id)
+    files = Dataset.objects.filter(project=prjct)
+    hasFiles = False
+    if len(files) > 0:
+        hasFiles = True
+    context = {
+        'project': prjct,
+        'hasFiles': hasFiles,
+        'files': files
+    }
+    return render(request, 'project.html', context)
 
 
 @login_required(login_url='login/')
 def datasets(request):
     context = {}
     return render(request, 'datasets.html', context)
+
+
+@login_required(login_url='login/')
+def feed_text(request):
+    context = {}
+
+
+@login_required(login_url='login/')
+def upload_file(request):
+    return None
 
 
 @login_required(login_url='login/')
@@ -142,3 +173,27 @@ def forgotpass_view(request):
                 'form': form,
             }
         return render(request, 'forgotpass.html', context)
+
+
+def add_project(request):
+    if request.method == 'POST':
+        user_email = request.session['user_email']
+        curernt_user = MyUser.objects.filter(email__exact=user_email).first()
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        new_project = Project(name=name, description=description, owner=curernt_user)
+        new_project.save()
+        return redirect('/project/' + str(new_project.id))
+
+
+def delete_project(request, id):
+    project_to_delete = Project.objects.get(id=id)
+    project_to_delete.delete()
+    return redirect('/projects/')
+
+
+def delete_file(request, id):
+    file_to_delete = Dataset.objects.get(id=id)
+    project_id = file_to_delete.project.id
+    file_to_delete.delete()
+    return redirect('/project/' + str(project_id))
