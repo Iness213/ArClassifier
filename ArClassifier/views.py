2af529937bb9ea864544bced0d97ef088818792a
ilyes24 <ilyes24ghomrani@gmail.com>
2020-08-21 04:38:46 +0100

    re-format views.py

--- ArClassifier/views.py
+++ ArClassifier/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.files.storage import default_storage
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from ArClassifier.form import LogInForm, SignUpForm, ForgotPassForm
-from ArClassifier.models import MyUser, Project, Dataset, Keyword, Result
+from ArClassifier.models import MyUser, Project, Dataset, Keyword, Result, Classification, TrainingSet
 from Text_Classification.settings import BASE_DIR
+
+from .Preprocessing import finale_preprocess
+from .predictor import predict_knn, predict_svm, predict_naive_bayes
 
 UserModel = get_user_model()
 
@@ -22,14 +26,14 @@ def index(request):
         for project in user_projects_qs:
             project_datasets_qs = Dataset.objects.filter(project=project)
             number_of_datasets = number_of_datasets + len(project_datasets_qs)
+            project_classification_qs = Classification.objects.filter(project=project)
+            for classification in project_classification_qs:
+                classification_result_qs = Result.objects.filter(classification=classification)
+                number_of_saved_results = number_of_saved_results + len(classification_result_qs)
+                for result in classification_result_qs:
+                    keyword_project = Keyword.objects.filter(result=result)
+                    number_of_keywords = number_of_keywords + len(keyword_project)
 
-            for dataset in project_datasets_qs:
-                dataset_keywords_qs = Keyword.objects.filter(dataset=dataset)
-                number_of_keywords = number_of_keywords + len(dataset_keywords_qs)
-
-                for keyword in dataset_keywords_qs:
-                    keyword_results_qs = Result.objects.filter(keyword=keyword)
-                    number_of_saved_results = number_of_saved_results + len(keyword_results_qs)
         context = {
             'number_of_datasets': number_of_datasets,
             'number_of_saved_results': number_of_saved_results,
@@ -55,26 +59,80 @@ def projects(request):
 def project(request, id):
     prjct = Project.objects.get(id=id)
     files = Dataset.objects.filter(project=prjct)
+    classifications = Classification.objects.filter(project=prjct)
     hasFiles = False
+    hasClassification = False
     if len(files) > 0:
         hasFiles = True
+    if len(classifications) > 0:
+        hasClassification = True
     context = {
         'project': prjct,
         'hasFiles': hasFiles,
-        'files': files
+        'hasClassification': hasClassification,
+        'files': files,
+        'classifications': classifications
     }
     return render(request, 'project.html', context)
 
 
 @login_required(login_url='login/')
-def datasets(request):
-    context = {}
-    return render(request, 'datasets.html', context)
+def add_project(request):
+    if request.method == 'POST':
+        user_email = request.session['user_email']
+        curernt_user = MyUser.objects.filter(email__exact=user_email).first()
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        new_project = Project(name=name, description=description, owner=curernt_user)
+        new_project.save()
+        return redirect('/project/' + str(new_project.id))
 
 
 @login_required(login_url='login/')
-def feed_text(request):
-    context = {}
+def delete_project(request, id):
+    project_to_delete = Project.objects.get(id=id)
+    project_to_delete.delete()
+    return redirect('/projects/')
+
+
+@login_required(login_url='login/')
+def file(request, id):
+    file = Dataset.objects.get(id=id)
+    project = Project.objects.get(id=file.project.id)
+    name = file.name
+    path = file.path
+    with (open(path, 'r', encoding='UTF-8')) as reader:
+        file_content = reader.read()
+        reader.close()
+    context = {
+        'file_name': name,
+        'file_content': file_content,
+        'project_id': project.id,
+        'project_name': project.name
+    }
+    return render(request, 'file.html', context)
+
+
+@login_required(login_url='login/')
+def feed_text(request, id):
+    project = Project.objects.get(id=id)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        text = request.POST.get('text')
+        user_email = request.session['user_email']
+        curernt_user = MyUser.objects.filter(email__exact=user_email).first()
+        path = BASE_DIR+'/files/'+str(curernt_user.id)+'/'+name+'.txt'
+        f = open(path, 'w+', encoding='UTF-8')
+        f.write(text)
+        file = Dataset(name=name+'.txt', project=project, path=path)
+        file.save()
+        return redirect('/project/' + str(id))
+
+    context = {
+        'projectId': project.id,
+        'project_name': project.name
+    }
+    return render(request, 'feedText.html', context)
 
 
 @login_required(login_url='login/')
@@ -95,21 +153,86 @@ def upload_file(request, id):
 
 
 @login_required(login_url='login/')
-def classification(request):
-    context = {}
+def delete_file(request, id):
+    file_to_delete = Dataset.objects.get(id=id)
+    project_id = file_to_delete.project.id
+    file_to_delete.delete()
+    return redirect('/project/' + str(project_id))
+
+
+@login_required(login_url='login/')
+def download_file(request, id):
+    file = Dataset.objects.get(id=id)
+    path = file.path
+    with(open(path, 'r')) as reader:
+        file_content = reader.read()
+        response = HttpResponse(file_content, content_type='application/octet-stream')
+        response['Content-Disposition'] = 'attachment; filename=' + file.name
+        reader.close()
+        return response
+
+
+@login_required(login_url='login/')
+def preprocess_text(request, id):
+    file = Dataset.objects.get(id=id)
+    path = file.path
+    with(open(path, 'r', encoding='UTF-8')) as reader:
+        file_content = reader.read()
+        reader.close()
+    pre_processed_text = finale_preprocess(file_content)
+    context = {
+        'project': file.project,
+        'pre_processed_text': pre_processed_text,
+        'file_content': file_content,
+        'file': file
+    }
+    return render(request, 'preprocess.html', context)
+
+
+@login_required(login_url='login/')
+def classification(request, id):
+    project = Project.objects.get(id=id)
+    datasets = TrainingSet.objects.all()
+    files = Dataset.objects.filter(project_id=id)
+    algorithms = {'KNN', 'SVM', 'Naive Bayes'}
+    if request.method == 'POST':
+        dataset = request.POST.get('dataset')
+        file_id = request.POST.get('file')
+        f = Dataset.objects.get(id=file_id)
+        with (open(f.path, 'r', encoding='UTF-8')) as reader:
+            file_content = reader.read()
+            reader.close()
+        algorithm = request.POST.get('algorithm')
+        if algorithm == 'KNN':
+            prediction_result, evaluation_result = predict_knn(file_content)
+        elif algorithm == 'SVM':
+            prediction_result, evaluation_result = predict_svm(file_content)
+        elif algorithm == 'Naive Bayes':
+            prediction_result, evaluation_result = predict_naive_bayes(file_content)
+        
+        return redirect('/project/' + str(id))
+    context = {
+        'project': project,
+        'datasets': datasets,
+        'files': files,
+        'algorithms': algorithms
+    }
     return render(request, 'classification.html', context)
 
 
 @login_required(login_url='login/')
-def saved_results(request):
-    context = {}
-    return render(request, 'savedResults.html', context)
-
-
-@login_required(login_url='login/')
-def help(request):
-    context = {}
-    return render(request, 'help.html', context)
+def result(request, id):
+    classification = Classification.objects.get(id=id)
+    result = Result.objects.get(classification_id=classification.id)
+    keywords = Keyword.objects.get(result=result)
+    project = classification.project
+    context = {
+        'project': project,
+        'result': result,
+        'classification': classification,
+        'keywords': keywords
+    }
+    return render(request, 'result.html', context)
 
 
 def login_view(request):
@@ -187,27 +310,3 @@ def forgotpass_view(request):
                 'form': form,
             }
         return render(request, 'forgotpass.html', context)
-
-
-def add_project(request):
-    if request.method == 'POST':
-        user_email = request.session['user_email']
-        curernt_user = MyUser.objects.filter(email__exact=user_email).first()
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        new_project = Project(name=name, description=description, owner=curernt_user)
-        new_project.save()
-        return redirect('/project/' + str(new_project.id))
-
-
-def delete_project(request, id):
-    project_to_delete = Project.objects.get(id=id)
-    project_to_delete.delete()
-    return redirect('/projects/')
-
-
-def delete_file(request, id):
-    file_to_delete = Dataset.objects.get(id=id)
-    project_id = file_to_delete.project.id
-    file_to_delete.delete()
-    return redirect('/project/' + str(project_id))
